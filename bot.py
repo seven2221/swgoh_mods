9c8738424c781dbdba5274292d0aca17ea56ed05
seven2221 <seven2221.93@gmail.com>
2024-04-03 04:00:10 +0300

    fix param3

--- bot.py
+++ bot.py
@@ -32,37 +32,38 @@ def choose_param1(message):
     elif param1 == 'круг':
         param2_options = ['%Здоровья', '%Защиты']
     else:  # треугольник
-        param2_options = ['Критшанс', 'Критурон', '%Атаки', '%Обороны', '%Здоровья', '%Защиты']
+        param2_options = ['%Критшанс', 'Критурон', '%Атаки', '%Обороны', '%Здоровья', '%Защиты']
 
     markup2 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     for option in param2_options:
         markup2.add(option)
     bot.send_message(message.chat.id, "Выберите характеристику модуля:", reply_markup=markup2)
 
-@bot.message_handler(func=lambda message: message.text in ['%Здоровья', '%Защиты', 'Скорость', '%Атаки', '%Обороны', 'Избегание крита', 'Критшанс', 'Критурон', 'Эффективность', 'Стойкость'])
+@bot.message_handler(func=lambda message: message.text in ['%Здоровья', '%Защиты', 'Скорость', '%Атаки', '%Обороны', 'Избегание крита', '%Критшанс', 'Критурон', 'Эффективность', 'Стойкость'])
 def choose_param2(message):
     global param2
     param2 = message.text.lower()
     
     markup3 = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    param3_options = ['Здоровье', 'Оборона', 'Критурон', 'Критшанс', 'Стойкость', 'Атака', 'Эффективность', 'Скорость']
+    param3_options = ['Здоровье', 'Оборона', 'Критический урон', 'Критшанс', '%Стойкости', 'Атака', '%Эффективности', '%Скорости']
     for option in param3_options:
         markup3.add(option)
     bot.send_message(message.chat.id, "Выберите сет модуля:", reply_markup=markup3)
 
-@bot.message_handler(func=lambda message: message.text in ['Здоровье', 'Оборона', 'Критурон', 'Критшанс', 'Стойкость', 'Атака', 'Эффективность', 'Скорость'])
+@bot.message_handler(func=lambda message: message.text in ['Здоровье', 'Оборона', 'Критический урон', 'Критшанс', '%Стойкости', 'Атака', '%Эффективности', '%Скорости'])
 def choose_param3(message):
-    param3_dict = {'Здоровье': 1, 'Оборона': 3, 'Критурон': 6, 'Критшанс': 5, 'Стойкость': 8, 'Атака': 2, 'Эффективность': 7, 'Скорость': 4}
-    param3 = param3_dict[message.text]
+    global param3
+    param3 = message.text.lower()
 
     # Формирование и выполнение запроса SQL
     query_param1 = {'стрелка': 'arrow', 'крест': 'cross', 'треугольник': 'triangle', 'круг': 'circle'}
     query_param2 = {'скорость': 5, '%атаки': 48, '%обороны': 49, '%здоровья': 55, '%защиты': 56, 'избегание крита': 54,
-                    'критшанс': 53, 'критурон': 16, 'эффективность': 17, 'стойкость': 9}
-    query_param3 = {'атака': 2, 'эффективность': 7, 'скорость': 4, 'здоровье': 1, 'стойкость': 8, 'критшанс': 5,
-                    'критурон': 6, 'оборона': 3}
+                    '%критшанс': 53, 'критурон': 16, 'эффективность': 17, 'стойкость': 9}
+    query_param3 = {'атака': 2, '%эффективности': 7, '%скорости': 4, 'здоровье': 1, '%стойкости': 8, 'критшанс': 5,
+                    'критический урон': 6, 'оборона': 3}
 
-    sql_query = f"SELECT char_name FROM chars WHERE `{query_param1[param1]}`='{query_param2[param2]}' and `sets` like '%{query_param3[message.text.lower()]}%'"
+    sql_query = f"SELECT char_name FROM chars WHERE `{query_param1[param1]}`='{query_param2[param2]}' and `sets` like '%{query_param3[param3]}%'"
+    print(sql_query)
     db_cursor.execute(sql_query)
 
     results = db_cursor.fetchall()  # Получаем результаты из базы данных
